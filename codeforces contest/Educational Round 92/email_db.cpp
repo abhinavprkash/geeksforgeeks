09b348110d5c1c6af1e7faba6be2da03d2a453ae
Abhinav <abhip13th@gmail.com>
2020-10-01 07:58:10 +0530

    new commit

--- codeforces contest/Educational Round 92/email_db.cpp
+++ codeforces contest/Educational Round 92/email_db.cpp
@@ -1,37 +1,45 @@
 
-	#include <bits/stdc++.h>
-	#include <cstdio>
-	#include <cstring>
-	#include <cmath>
-	#include <cstring>
-	#include <chrono>
-	#include <complex>
-	#define endl "\n"
-	#define ll long long int
-	#define vi vector<int>
-	#define vll vector<ll>
-	#define vvi vector < vi >
-	#define pii pair<int,int>
-	#define pll pair<long long, long long>
-	#define mod 1000000007
-	#define inf 1000000000000000001;
-	#define all(c) c.begin(),c.end()
-	#define mp(x,y) make_pair(x,y)
-	#define mem(a,val) memset(a,val,sizeof(a))
-	#define eb emplace_back
-	#define f first
-	#define s second
-	
-	using namespace std;
-	int main()
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+int main()
+{
+	freopen("output2.out", "w", stdout);
+	int n;
+	cin >> n;
+	for (int i = 0; i <= 9; ++i)
 	{
-		std::ios::sync_with_stdio(false);
-		freopen("output2.txt", "w", stdout);
-		int n;
-		cin>>n;
-		for (int i = 1; i <= 9; ++i)
-		{
-			cout<<"190600"<<i<<"@kiit.ac.in"<<endl;
-		}
-		return 0;
+		cout << "190500" << i << "@kiit.ac.in" << endl;
 	}
+	for (int i = 10; i <= 99; ++i)
+	{
+		cout << "19050" << i << "@kiit.ac.in" << endl;
+	}
+	for (int i = 100; i <= 999; ++i)
+	{
+		cout << "1905" << i << "@kiit.ac.in" << endl;
+	}
+
+	return 0;
+}
